9d3dcbefbaab574a84d1c575682e275d357b25c5
JustM57 <tiir57@yandex.ru>
2020-09-18 11:42:11 +0300

    hw1

--- classify double/solution.c
+++ classify double/solution.c
@@ -15,16 +15,26 @@ uint64_t convertToUint64 (double number) {
 }
 
 bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+    uint32_t mask = 1 << index;
+    uint32_t temp = mask & number;
+    temp >>= index;
+    return temp;
 }
 
-
+uint8_t countNBits (const uint64_t number, const uint8_t start_index,
+                    const uint8_t end_index) {
+    uint8_t counter = 0;
+    for (uint8_t bit = start_index; bit <= end_index; ++bit) {
+        if getBit(number, bit) ++counter;
+    }
+    return counter;
+}
 /**
  * Checkers here:
  */
 
 bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+    return number == 0x0000000000000000;
 }
 
 bool checkForMinusZero (uint64_t number) {
@@ -32,35 +42,50 @@ bool checkForMinusZero (uint64_t number) {
 }
 
 bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0x7FF0000000000000;
 }
 
 bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0xFFF0000000000000;
+}
+
+bool checkForNormal (uint64_t number) {
+    //count exponent bits
+    uint8_t counter = countNBits(number, 1, 11);
+    return ((counter > 0) && (counter < 11))
 }
 
 bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+    return (!getBit(number, 0) && checkForNormal(number))
 }
 
 bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+    return (getBit(number, 0) && checkForNormal(number))
+}
+
+bool checkForDenormal (uint64_t number) {
+    uint8_t exp_bit = countNBits(number, 1, 11);
+    uint8_t frac_bit = countNBits(number, 12, 63);
+    return ((exp_bit == 0) && (frac_bit > 1));
 }
 
 bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+    return (!getBit(number, 0) &&checkForDenormal(number));
 }
 
 bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+    return (getBit(number, 0) &&checkForDenormal(number));
 }
 
 bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+    uint8_t exp_bit = countNBits(number, 0, 11);
+    uint8_t fbit = getBit(number, 12);
+    return ((exp_sum == 12) && !fbit);
 }
 
 bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+    uint8_t bits_sum = countNBits(number, 0, 12);
+    return (bits_sum == 13);
 }
 
 
