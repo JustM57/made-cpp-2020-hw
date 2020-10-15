4efe655e3dc4f17de9b723e956ce6f861b7f80f6
JustM57 <tiir57@yandex.ru>
2020-10-15 12:06:38 +0300

    first 2 classes

---
+++ geometry/src/geometry.cpp
@@ -0,0 +1,4 @@
+//
+// Created by justm on 15.10.2020.
+//
+
--- geometry/src/geometry.h
+++ geometry/src/geometry.h
@@ -1,3 +1,16 @@
 #pragma once
+#include <cmath>
 
-// Your code here...
+namespace geometry {
+    const double EPS = 1e-6;
+
+    struct Point {
+        double x, y;
+        Point();
+        Point(double x, double y);
+        bool operator==(const Point& other) const;
+        bool operator!=(const Point& other) const;
+    };
+
+
+};
