950ed9edb6a36eff64c882ae39490f0175d4571a
JustM57 <tiir57@yandex.ru>
2020-10-18 14:28:55 +0300

    initial commit

--- geometry/src/geometry.h
+++ geometry/src/geometry.h
@@ -12,5 +12,17 @@ namespace geometry {
         bool operator!=(const Point& other) const;
     };
 
+    class Line {
+    private:
+        // a x + b y + c = 0
+        double a, b, c;
+    public:
+        Line(const Point& p1, const Point& p2);
+        Line(double k, double b);
+        Line(const Point& p, double k);
+        bool operator==(const Line& other) const;
+        bool operator!=(const Line& other) const;
+    };
+
 
 };
